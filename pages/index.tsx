b2406f24db68f3b42f48034deb72cbf97f1a3ab4
sakitayu <sakitakitakita@gmail.com>
2021-10-05 21:16:58 +0900

    react-bootstrapを導入＆ユーザー一覧をTableコンポーネントで表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import React, { useEffect, useState, FC } from 'react';
 import { User } from '@/Domain/Entity/User';
 import { UserRepository } from '@/Domain/Repository/UserRepository';
+import { Container, Table } from 'react-bootstrap';
 
 const Home: NextPage = () => {
     const [users, setUsers] = useState<User[]>([]);
@@ -14,7 +15,28 @@ const Home: NextPage = () => {
         });
     }, []);
 
-    return <div>ここにユーザー一覧</div>;
+    const trs = users.map(user => {
+        return (
+            <tr>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+            </tr>
+        );
+    });
+
+    return (
+        <Container className='mx-auto my-5'>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>{trs}</tbody>
+            </Table>
+        </Container>
+    );
 };
 
 export default Home;
