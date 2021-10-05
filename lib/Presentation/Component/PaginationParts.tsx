fbcb6df93b61882499da41f1a1d0c313c9dd6dde
sakitayu <sakitakitakita@gmail.com>
2021-10-05 22:14:44 +0900

    sassを導入＆PaginationPartsコンポーネントを追加

---
+++ lib/Presentation/Component/PaginationParts.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Pagination } from 'react-bootstrap';
+import Router from 'next/router';
+interface Props {
+    totalPages: number;
+    currentPage: number;
+    targetPagePath: string;
+}
+
+const totalPageIndications = 10;
+const middlePagePosition = Math.floor(totalPageIndications / 2) + 1;
+
+export const PaginationParts: React.FC<Props> = props => {
+    const paginationItems = [...Array(totalPageIndications)].map((e, key) => {
+        const pageNumber = key + 1;
+        const pageNumberAroundMiddlePage =
+            pageNumber + props.currentPage - middlePagePosition;
+        const pageNumberUpToLastPage =
+            pageNumber + props.totalPages - totalPageIndications;
+        if (pageNumber > props.totalPages) return null;
+        if (
+            props.currentPage <= middlePagePosition ||
+            props.totalPages <= totalPageIndications
+        ) {
+            return (
+                <Pagination.Item
+                    key={pageNumber}
+                    onClick={() =>
+                        Router.push(props.targetPagePath + pageNumber)
+                    }
+                    active={pageNumber === props.currentPage}
+                    className='mx-1'
+                >
+                    {pageNumber}
+                </Pagination.Item>
+            );
+        } else if (
+            props.totalPages > totalPageIndications &&
+            props.currentPage >= middlePagePosition &&
+            props.currentPage + middlePagePosition <= props.totalPages
+        ) {
+            return (
+                <Pagination.Item
+                    key={pageNumberAroundMiddlePage}
+                    onClick={() =>
+                        Router.push(
+                            props.targetPagePath + pageNumberAroundMiddlePage,
+                        )
+                    }
+                    active={pageNumberAroundMiddlePage === props.currentPage}
+                    className='mx-1'
+                >
+                    {pageNumberAroundMiddlePage}
+                </Pagination.Item>
+            );
+        } else if (
+            props.totalPages > totalPageIndications &&
+            props.currentPage + middlePagePosition > props.totalPages
+        ) {
+            return (
+                <Pagination.Item
+                    key={pageNumberUpToLastPage}
+                    onClick={() =>
+                        Router.push(
+                            props.targetPagePath + pageNumberUpToLastPage,
+                        )
+                    }
+                    active={pageNumberUpToLastPage === props.currentPage}
+                    className='mx-1'
+                >
+                    {pageNumberUpToLastPage}
+                </Pagination.Item>
+            );
+        }
+    });
+
+    return (
+        <Pagination className='align-items-center'>
+            <Pagination.First
+                onClick={() =>
+                    props.currentPage !== 1 &&
+                    Router.push(props.targetPagePath + 1)
+                }
+                className='mx-1 page-angle'
+            />
+            <Pagination.Prev
+                onClick={() =>
+                    props.currentPage !== 1 &&
+                    Router.push(props.targetPagePath + (props.currentPage - 1))
+                }
+                className='mx-1 page-angle'
+            />
+            {paginationItems}
+            <Pagination.Next
+                onClick={() =>
+                    props.currentPage !== props.totalPages &&
+                    Router.push(props.targetPagePath + (props.currentPage + 1))
+                }
+                className='mx-1 page-angle'
+            />
+            <Pagination.Last
+                onClick={() =>
+                    props.currentPage !== props.totalPages &&
+                    Router.push(props.targetPagePath + props.totalPages)
+                }
+                className='mx-1 page-angle'
+            />
+        </Pagination>
+    );
+};
