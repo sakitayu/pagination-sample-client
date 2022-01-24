b3acc67b1706a2bda66f292da86cb8a5e068c542
sakitayu <sakitakitakita@gmail.com>
2022-01-24 17:52:14 +0900

    アーキテクチャをめちゃくちゃシンプルに

--- lib/Domain/Entity/User.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    constructor(
-        public id: number,
-        public name: string,
-        public createdAt: Date,
-        public updatedAt: Date,
-    ) {}
-}
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,10 +1,48 @@
 import type { NextPage, GetServerSideProps } from 'next';
 import React, { useEffect, useState, FC } from 'react';
-import { User } from '@/Domain/Entity/User';
-import { UserRepository } from '@/Domain/Repository/UserRepository';
-import { PaginationParts } from '@/Presentation/Component/PaginationParts';
+import { PaginationParts } from 'component/PaginationParts';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { Container, Table } from 'react-bootstrap';
 
+class RestClient {
+    constructor(private readonly token: string) {}
+
+    async get<ResponseInterface>(
+        path: string,
+    ): Promise<AxiosResponse<ResponseInterface>> {
+        return await axios.get<ResponseInterface>(path, this.requestConfig);
+    }
+
+    private get requestConfig(): AxiosRequestConfig {
+        return {
+            baseURL: 'http://localhost:3000',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: this.token ? `Bearer ${this.token}` : '',
+            },
+        };
+    }
+}
+
+export function getNoAuthClient(): RestClient {
+    return new RestClient('');
+}
+
+class User {
+    constructor(
+        public id: number,
+        public name: string,
+        public createdAt: Date,
+        public updatedAt: Date,
+    ) {}
+}
+
+interface UserResponseObject {
+    id: number;
+    name: string;
+    created_at: string;
+    updated_at: string;
+}
 interface Props {
     currentPage: number;
 }
@@ -14,11 +52,23 @@ const UsersIndex: NextPage<Props> = ({ currentPage }) => {
     const [totalPages, setTotalPages] = useState<number>();
 
     useEffect(() => {
-        UserRepository.index(currentPage).then(res => {
-            setUsers(res.data);
-            setTotalPages(res.totalPages);
-        });
+        fetchDate();
     }, [currentPage]);
+
+    const fetchDate = async () => {
+        const res = await getNoAuthClient().get<UserResponseObject[]>(
+            `/users?page=${currentPage}`,
+        );
+        const users = (res: UserResponseObject) =>
+            new User(
+                res.id,
+                res.name,
+                new Date(res.created_at),
+                new Date(res.updated_at),
+            );
+        setUsers(res.data.map(users));
+        setTotalPages(Number(res.headers['total-pages']));
+    };
 
     const trs = users.map(user => {
         return (
